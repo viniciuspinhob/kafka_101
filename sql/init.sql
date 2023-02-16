1efea8f494c526674d05054f01d501b359ec4720
Vinicius Barbosa <vpbarbosa@ihm.com.br>
2023-02-16 13:12:47 -0300

    add docker-compose and sql files

---
+++ sql/init.sql
@@ -0,0 +1,20 @@
+
+--  Create Postgree table
+CREATE TABLE IF NOT EXISTS process_data (
+    timestamp TIMESTAMPTZ NOT NULL,
+    tagname TEXT NOT NULL,
+    value DOUBLE PRECISION NOT NULL,
+    quality TEXT NOT NULL
+);
+
+--  Convert table to TimeScaleDB
+-- SELECT create_hypertable('process_data', 'timestamp');
+
+-- Set index to ID
+-- CREATE INDEX ON process_data (tmstp DESC, id)
+--     WHERE id IS NOT NULL;
+
+
+
+
+-- Criar um atable apor TAG pver vom breno
